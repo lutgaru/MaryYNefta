30bbe0b8e3f835bfdde7ce79a172f91199fe4ba0
lutgaru <lutgaru@gmail.com>
2025-06-05 21:47:48 -0700

    fix add to calendar button

---
+++ src/components/Calendar.vue
@@ -0,0 +1,23 @@
+
+<template>
+<add-to-calendar-button
+  name="Title"
+  options="'Apple','Google','iCal','Outlook.com','Microsoft 365','Microsoft Teams'"
+  location="https://maps.app.goo.gl/pQ4A72C1JCdg2ZXo7"
+  startDate="2026-01-03"
+  endDate="2026-01-03"
+  startTime="01:00"
+  endTime="23:59"
+  timeZone="Mexico/General" 
+  iCalFileName="invitacionmarynefta"
+  class="p-2"
+    styleLight="--btn-background: #e5e7eb; --btn-border: 2px solid #d1d5dc;  --btn-text: #364153; --font: Georgia, 'Times New Roman', Times, serif;" 
+></add-to-calendar-button>
+</template>
+
+
+<script>
+
+import 'add-to-calendar-button';
+
+</script>
